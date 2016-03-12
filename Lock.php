7f3700629aae645b47df8d5cb4f234aa74ae86c4
wdst <wds@mail.ru>
2016-03-13 03:15:25 +0400

    init

---
+++ Lock.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace wdst\lock;
+
+Class Lock {
+
+    private $lockfile;
+
+    public function __construct($lockfile)
+    {
+        $this->lockfile = $lockfile;
+        $this->check();
+    }
+
+    public function check()
+    {
+        if(!realpath(dirname($this->lockfile))){
+            throw new LockException("Lockfile folder don't exists: " . $this->lockfile);
+        }
+
+        if(!is_writable(dirname($this->lockfile))){
+            throw new LockException("Lockfile folder don't writable: " . $this->lockfile);
+        }
+    }
+
+    public function is_lock()
+    {
+        return file_exists($this->lockfile);
+    }
+
+    public function lock()
+    {
+        $this->check();
+        if(file_exists($this->lockfile) && !is_writable($this->lockfile)){
+            throw new LockException("Lockfile don't writable: " . $this->lockfile);
+            return false;
+        }
+        file_put_contents('$this->lockfile', $this->getInfo());
+        return true;
+    }
+
+    public function unlock()
+    {
+        if($this->is_lock()){
+            unlink($this->lockfile);
+        }
+        return true;
+    }
+
+    public function time()
+    {
+        if($this->is_lock()){
+            $info = file_get_contents($this->lockfile);
+            return $info;
+        }
+        return false;
+    }
+
+    /**
+     * Text for writing into lockfile
+     * use date
+     *
+     * @return string
+     */
+
+    private function getInfo()
+    {
+        return date('d-m-Y H:i:s');
+    }
+}
---
+++ LockException.php
@@ -0,0 +1,6 @@
+<?php
+
+namespace wdst\lock;
+
+Class LockException extends \Exception {}
+
